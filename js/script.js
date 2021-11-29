77150c2b6f41e612821ee58ef726db63e11fe443
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2021-11-29 11:14:07 +0100

    Add 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.active');
 
-    for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-    }
+  for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
